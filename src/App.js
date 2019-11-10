92289a85010d4b9d8eb97d7a9bd0be7c5a3600c6
Sergey <halygali@mail.ru>
2019-11-10 12:24:22 +0400

    add fon

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import logo1 from './logo1.jpg';
+import image3 from './image3.jpg';
 import './App.css';
 
 function App() {
@@ -7,7 +8,7 @@ function App() {
     <div className="App">
       <div className="parent">
         <div className="A">
-        <div class="base-m">
+        <div className="base-m">
           <div className="menu">
             <div className="m1"><a href="#">Главная</a></div>
             <div className="m2"><a href="#">Скилы</a></div>
@@ -17,29 +18,32 @@ function App() {
         </div>
         </div>
         <div className="B">
-          <div class="baseB">
+          <div className="baseB">
             <div className="resume">
               <span>Привет!<br/> Меня зовут Петров Сергей.<br/> Я front-and разработчик.</span>
             </div>
             <div className="foto">
-              <img src={logo1} alt="this is Logo1 image"/>
             </div>
           </div>
         </div>
         <div className="C">
-          <div class="baseC">
+          <div className="baseC">
             <div className="skill">
               <span>Мои скилы</span>
             </div>
             <div className="info">
-              <div className="i1"></div>
+              <div className="i1">
+              </div>
               <div className="i2">
-                <div className="i21"></div>
-                <div className="i22"></div>
+                <div className="i21">
+                </div>
+                <div className="i22">
+                </div>
               </div>
               <div className="i3">
                 <div className="i31">
-                  <div className="i311"></div>
+                  <div className="i311">
+                  </div>
                   <div className="i312">
                     <span>React</span>
                   </div>
@@ -52,14 +56,16 @@ function App() {
           </div>
         </div>
         <div className="D">
-          <div class="baseD">
+          <div className="baseD">
             <div className="job">
               <span>Мои работы</span>
             </div>
             <div className="examples">
               <div className="example1">
-                <div className="example11"></div>
-                <div className="example12"></div>
+                <div className="example11">
+                </div>
+                <div className="example12">
+                </div>
               </div>
               <div className="example2">
                 <div className="example21">
@@ -74,13 +80,19 @@ function App() {
         </div>
         </div>
         <div className="E">
-          <div class="base"></div>
+          <div className="baseE">
+
+          </div>
         </div>
         <div className="F">
-          <div class="base"></div>
+          <div className="baseF">
+
+          </div>
         </div>
         <div className="G">
-          <div class="base"></div>
+          <div className="baseG">
+
+          </div>
         </div>
       </div>
     </div>
