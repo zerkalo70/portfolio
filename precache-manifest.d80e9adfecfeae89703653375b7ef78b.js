16eb49dd3fbc7e01cef61c687547a9df05db4dcb
Sergey <halygali@mail.ru>
2019-11-11 16:55:10 +0400

    Updates

---
+++ precache-manifest.d80e9adfecfeae89703653375b7ef78b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "853d196f3a9c20cd971ed8fb4049874e",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "cf4e28ca33e28c4d8687",
+    "url": "/portfolio/static/css/main.23da9c03.chunk.css"
+  },
+  {
+    "revision": "101ada89f1ab1d6fd541",
+    "url": "/portfolio/static/js/2.598d69a0.chunk.js"
+  },
+  {
+    "revision": "cf4e28ca33e28c4d8687",
+    "url": "/portfolio/static/js/main.5c4d7b82.chunk.js"
+  },
+  {
+    "revision": "76aa8cbd67b5c937ec2c",
+    "url": "/portfolio/static/js/runtime-main.e92c632a.js"
+  },
+  {
+    "revision": "ae71c89f8aa85ccb0d3aaaa6530d3761",
+    "url": "/portfolio/static/media/image3.ae71c89f.jpg"
+  },
+  {
+    "revision": "67c91fb1a6a59db67a69067f97934d67",
+    "url": "/portfolio/static/media/logo1.67c91fb1.jpg"
+  }
+]);
