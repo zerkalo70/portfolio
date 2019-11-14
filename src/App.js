3496159e42d0cb8c3ad9a00ff71fe623f03393a8
Sergey <halygali@mail.ru>
2019-11-14 20:46:06 +0400

    made block2

--- src/App.js
+++ src/App.js
@@ -20,13 +20,33 @@ const App=()=>{
                             <div className="basis11">
                                 <span>Привет!<br/> Меня зовут Петров Сергей.<br/> Я front-and разработчик.</span>
                             </div>
-                            <div className="basis12">
-                                <span></span>
-                            </div>
+                            <div className="basis12"></div>
                         </div>
                     </div>
                     <div className="block-2">Блок 2
-                        <div className="basis2">база-2</div>
+                        <div className="basis2">
+                            <div className="basis21">
+                                <span>Мои скилы</span>
+                            </div>
+                            <div className="basis22">
+                                <div className="basis22a"></div>
+                                <div className="basis22b">
+                                    <div className="bb1"></div>
+                                    <div className="bb2"></div>
+                                </div>
+                                <div className="basis22c">
+                                    <div className="bc1">
+                                        <div className="bc11"></div>
+                                        <div className="bc12">
+                                            <span>React</span>
+                                        </div>
+                                    </div>
+                                    <div className="bc2">
+                                        <span>Подробное описание навыка</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                     <div className="block-3">Блок 3
                         <div className="basis3">база-3</div>
