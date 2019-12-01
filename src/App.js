59c3d664a23c88edc41087644efcd9b6e24057ea
Sergey <halygali@mail.ru>
2019-12-01 23:02:04 +0300

    edit all

--- src/App.js
+++ src/App.js
@@ -1,120 +1,15 @@
 import React from 'react';
 import './App.css';
+import Header from "./Components/Header/Header";
+import Content from "./Components/Content/Content";
+import Footer from "./Components/Footer/Footer";
 
-const App=()=>{
-    return (
-            <div className="Parent">
-               <header className="Header">
-                   <div className="block-menu">
-            {/*<div className="menu">*/}
-                            <div className="a1"><a href="#">Главная</a></div>
-                            <div className="a2"><a href="#">Скилы</a></div>
-                            <div className="a3"><a href="#">Работы</a></div>
-                            <div className="a4"><a href="#">Контакты</a></div>
-            {/*        /!*    /!*</div>*!/*!/*/}
-                   </div>
-               </header>
-               <div className="Content">Content
-                   <div className="block-1">Блок 1
-            {/*        /!*    <div className="basis1">*!/*/}
-            {/*        /!*        <div className="basis11">*!/*/}
-            {/*        /!*            <span>Привет!<br/> Меня зовут Петров Сергей.<br/> Я front-end разработчик.</span>*!/*/}
-            {/*        /!*        </div>*!/*/}
-            {/*        /!*        <div className="basis12"></div>*!/*/}
-            {/*        /!*    </div>*!/*/}
-                  </div>
-                  <div className="block-2">Блок 2
-            {/*        /!*    <div className="basis2">*!/*/}
-            {/*        /!*        <div className="basis21">*!/*/}
-            {/*        /!*            <span>Мои скилы</span>*!/*/}
-            {/*        /!*        </div>*!/*/}
-            {/*        /!*        <div className="basis22">*!/*/}
-            {/*        /!*            <div className="basis22a"></div>*!/*/}
-            {/*        /!*            <div className="basis22b">*!/*/}
-            {/*        /!*                <div className="bb1"></div>*!/*/}
-            {/*        /!*                <div className="bb2"></div>*!/*/}
-            {/*        /!*            </div>*!/*/}
-            {/*        /!*            <div className="basis22c">*!/*/}
-            {/*        /!*                <div className="bc1">*!/*/}
-            {/*        /!*                    <div className="bc11"></div>*!/*/}
-            {/*        /!*                    <div className="bc12">*!/*/}
-            {/*        /!*                        <span>React</span>*!/*/}
-            {/*        /!*                    </div>*!/*/}
-            {/*        /!*                </div>*!/*/}
-            {/*        /!*                <div className="bc2">*!/*/}
-            {/*        /!*                    <span>Подробное описание навыка</span>*!/*/}
-            {/*        /!*                </div>*!/*/}
-            {/*        /!*            </div>*!/*/}
-            {/*        /!*        </div>*!/*/}
-            {/*        /!*    </div>*!/*/}
-                  </div>
-                   <div className="block-3">Блок 3
-            {/*        /!*    <div className="basis3">*!/*/}
-            {/*        /!*        <div className="basis31">*!/*/}
-            {/*        /!*            <span>Мои работы</span>*!/*/}
-            {/*        /!*        </div>*!/*/}
-            {/*        /!*        <div className="basis32">*!/*/}
-            {/*        /!*            <div className="basis32a">*!/*/}
-            {/*        /!*                <div className="ba321">*!/*/}
-            {/*        */}
-            {/*        /!*                </div>*!/*/}
-            {/*        /!*                <div className="ba322">*!/*/}
-            {/*        */}
-            {/*        /!*                </div>*!/*/}
-            {/*        /!*            </div>*!/*/}
-            {/*        /!*            <div className="basis32b">*!/*/}
-            {/*        /!*                <div className="bb321">*!/*/}
-            {/*        /!*                    <button className="but321">Смотреть</button>*!/*/}
-            {/*        /!*                </div>*!/*/}
-            {/*        /!*                <div className="bb322">*!/*/}
-            {/*        /!*                    <div className="bt322a"><span>Название проекта</span></div>*!/*/}
-            {/*        /!*                    <div className="bt322b"><span>Краткое описание</span></div>*!/*/}
-            {/*        /!*                </div>*!/*/}
-            {/*        /!*            </div>*!/*/}
-            {/*        /!*        </div>*!/*/}
-            {/*        /!*    </div>*!/*/}
-                   </div>
-                   <div className="block-4">Блок 4
-            {/*        /!*    <div className="basis4">*!/*/}
-            {/*        /!*        <div className="b41">*!/*/}
-            {/*        /!*            <span>Рассматриваю варианты удалённой работы</span>*!/*/}
-            {/*        /!*        </div>*!/*/}
-            {/*        /!*        <div className="b42">*!/*/}
-            {/*        /!*            <span>Нанять меня</span>*!/*/}
-            {/*        /!*        </div>*!/*/}
-            {/*        /!*    </div>*!/*/}
-                   </div>
-                   <div className="block-5">Блок 5
-            {/*        /!*    <div className="basis5">*!/*/}
-            {/*        /!*        <div className="bas51">*!/*/}
-            {/*        /!*            <span>Контакты</span>*!/*/}
-            {/*        /!*        </div>*!/*/}
-            {/*        /!*        <div className="bas52">*!/*/}
-            {/*        /!*            <div className="b521"></div>*!/*/}
-            {/*        /!*            <div className="b522"></div>*!/*/}
-            {/*        /!*            <div className="b523"></div>*!/*/}
-            {/*        /!*        </div>*!/*/}
-            {/*        /!*        <div className="bas53">Отправить</div>*!/*/}
-            {/*        /!*    </div>*!/*/}
-                   </div>
-               </div>
-              <footer className="Footer">Footer
-            {/*    /!*    <div className="basisF">*!/*/}
-            {/*    /!*            <div className="bF1">*!/*/}
-            {/*    /!*                <span>Сергей Петров</span>*!/*/}
-            {/*    /!*            </div>*!/*/}
-            {/*    /!*            <div className="bF2">*!/*/}
-            {/*    /!*                <div className="bF21"></div>*!/*/}
-            {/*    /!*                <div className="bF22"></div>*!/*/}
-            {/*    /!*                <div className="bF23"></div>*!/*/}
-            {/*    /!*            </div>*!/*/}
-            {/*    /!*            <div className="bF3">*!/*/}
-            {/*    /!*                <span>© 2019 Все права защищены</span>*!/*/}
-            {/*    /!*            </div>*!/*/}
-            {/*    /!*    </div>*!/*/}
-               </footer>
-            </div>
-        );
+const App = () => {
+    return <div className="Parent">
+        <Header />
+        <Content />
+        <Footer />
+    </div>
 }
 
 export default App;
