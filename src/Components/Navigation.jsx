023322b925648f6ed6f5dc1c75bed04940774536
Sergey <halygali@mail.ru>
2020-01-02 07:51:36 +0300

    edded color2

--- src/Components/Navigation.jsx
+++ src/Components/Navigation.jsx
@@ -7,19 +7,19 @@ const Navigation = () => {
     return (
         <nav className={n.nav}>
             <div className={n.nav1}>
-                <NavLink to = "/Resume">Главная</NavLink>
+                <NavLink to = "/Resume" activeClassName={n.active}>Главная</NavLink>
             </div>
             <div className={n.nav2}>
-                <NavLink to = "/Skills">Скилы</NavLink>
+                <NavLink to = "/Skills" activeClassName={n.active}>Скилы</NavLink>
             </div>
             <div className={n.nav3}>
-                <NavLink to = "/Jobs">Работы</NavLink>
+                <NavLink to = "/Jobs" activeClassName={n.active}>Работы</NavLink>
             </div>
             <div className={n.nav4}>
-                <NavLink to = "/Long">Работодателям</NavLink>
+                <NavLink to = "/Long" activeClassName={n.active}>Работодателям</NavLink>
             </div>
             <div className={n.nav5}>
-                <NavLink to = "/Contacts">Контакты</NavLink>
+                <NavLink to = "/Contacts" activeClassName={n.active}>Контакты</NavLink>
             </div>
         </nav>
     )
