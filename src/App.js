b90d7d01f7aa8552fd946d1fbcdd8197bbf1a4b7
Sergey <halygali@mail.ru>
2020-01-19 12:04:37 +0300

    Begin start

--- src/App.js
+++ src/App.js
@@ -1,38 +1,19 @@
 import React from 'react';
 import './App.css';
-import logo1 from './logo1.jpg';
-import image3 from './Components/image3.jpg';
-import Navigation from "./Components/Navigation";
-import Resume from "./Components/Resume";
-import Skills from "./Components/Skills";
-import Jobs from "./Components/Jobs";
-import Long from "./Components/Long";
-import Contacts from "./Components/Contacts";
-import Footer from "./Components/Footer";
 import {BrowserRouter, Route} from "react-router-dom";
 
 
 const App = () => {
     return (
-        <BrowserRouter>
-    <div className="Parent">
-            <header className="header">
-              <Navigation/>
-            </header>
-        <Route path = '/Resume' component = {Resume}/>
-        <Route path = '/Skills' component = {Skills}/>
-        <Route path = '/Jobs' component = {Jobs}/>
-        <Route path = '/Contacts' component = {Contacts}/>
-        <Route path = '/Long' component = {Long}/>
-        {/*<Route path = '/Footer' component = {Footer}/>*/}
-            {/*<Resume/>*/}
-            {/*<Skills/>*/}
-            {/*<Jobs/>*/}
-            {/*<Long/>*/}
-            {/*<Contacts/>*/}
-            <Footer/>
+        <div className="GrossParent">
+            <div className="Navigation">NavBar</div>
+            <div className="Resume">Resume</div>
+            <div className="MyWorks">MyWorks</div>
+            <div className="MyServices">MyServices</div>
+            <div className="Freelanc">Freelanc</div>
+            <div className="Contacts">Contacts</div>
+            <div className="Footer">Footer</div>
         </div>
-        </BrowserRouter>
     )
 }
 
