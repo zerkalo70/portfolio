2aaa13fa1d576b7fbe47fa833d5f0eb5a70c32aa
Sergey <halygali@mail.ru>
2020-02-16 13:40:39 +0300

    edit works

--- src/App.js
+++ src/App.js
@@ -39,8 +39,27 @@ const App = () => {
                     </div>
                 </div>
             </div>
-            <div className="MyWorks"></div>
-            <div className="MyServices">MyServices</div>
+            <div className="MyWorks">
+                <div className="titleWorks">Мои работы</div>
+                <div className="wrap">
+                    <input type="radio" name="slides" id="point1" checked></input>
+                    <input type="radio" name="slides" id="point2"></input>
+                    <input type="radio" name="slides" id="point3"></input>
+                    <div className="slider">
+                        <div className="slides img1"></div>
+                        <div className="slides img2"></div>
+                        <div className="slides img3"></div>
+                    </div>
+                    <div className="control">
+                        <label htmlFor="point1"></label>
+                        <label htmlFor="point2"></label>
+                        <label htmlFor="point3"></label>
+                    </div>
+                </div>
+            </div>
+            <div className="MyServices">
+                <div className="titleServices">Мои навыки</div>
+            </div>
             <div className="Freelanc">Freelanc</div>
             <div className="Contacts">Contacts</div>
             <div className="Footer">Footer</div>
