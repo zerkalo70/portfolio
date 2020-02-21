84527a156f6bf7ae31717f16843568294631c90e
Sergey <halygali@mail.ru>
2020-02-21 08:18:30 +0300

    tuning modules css

--- src/Components/MyWorks.js
+++ src/Components/MyWorks.js
@@ -9,18 +9,20 @@ class MyWorks extends React.Component {
             <div className={s.MyWorks}>
                 <div className={s.titleWorks}>Мои работы</div>
                 <div className={s.wrap}>
-                    <input type="radio" name="slides" id="point1" checked></input>
-                    <input type="radio" name="slides" id="point2"></input>
-                    <input type="radio" name="slides" id="point3"></input>
+
+                    <input type="radio" name={s.slides} id={s.point1} checked></input>
+                    <input type="radio" name={s.slides} id={s.point2}></input>
+                    <input type="radio" name={s.slides} id={s.point3}></input>
+
                     <div className={s.slider}>
                         <div className={`${s.slides} ${s.img1}`}></div>
                         <div className={`${s.slides} ${s.img2}`}></div>
                         <div className={`${s.slides} ${s.img3}`}></div>
                     </div>
                     <div className={s.control}>
-                        <label htmlFor="point1"></label>
-                        <label htmlFor="point2"></label>
-                        <label htmlFor="point3"></label>
+                        <label htmlFor={s.point1}></label>
+                        <label htmlFor={s.point2}></label>
+                        <label htmlFor={s.point3}></label>
                     </div>
                 </div>
             </div>
