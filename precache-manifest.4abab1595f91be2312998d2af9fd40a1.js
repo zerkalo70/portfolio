d3591cc94e924ddcfbd933d36aa286c6817d2d0d
Sergey <halygali@mail.ru>
2020-04-12 18:31:08 +0300

    Updates

--- precache-manifest.4abab1595f91be2312998d2af9fd40a1.js
+++ precache-manifest.4abab1595f91be2312998d2af9fd40a1.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "5ca32907d38b430d2dd358ef30d0c250",
+    "revision": "e664e9eb7acf3dfc4920b3216e34a327",
     "url": "/portfolio/index.html"
   },
   {
-    "revision": "c2abcfee71fa50789335",
-    "url": "/portfolio/static/css/main.f8ca04cc.chunk.css"
+    "revision": "cc598b1f8d6d45efd6cc",
+    "url": "/portfolio/static/css/main.57a3f994.chunk.css"
   },
   {
     "revision": "e5046b794be4040b0535",
     "url": "/portfolio/static/js/2.a3b97a67.chunk.js"
   },
   {
-    "revision": "c2abcfee71fa50789335",
-    "url": "/portfolio/static/js/main.dea5b242.chunk.js"
+    "revision": "cc598b1f8d6d45efd6cc",
+    "url": "/portfolio/static/js/main.8872ce1e.chunk.js"
   },
   {
     "revision": "76aa8cbd67b5c937ec2c",
