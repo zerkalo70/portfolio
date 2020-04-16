fa12b2a4e670cbaeb26d39b5fcbf55cc3750a1ab
Сергей <halygali@mail.ru>
2020-04-16 23:32:20 +0300

    Updates

--- precache-manifest.7c48e96417f2ec9e06ac786eac5571e7.js
+++ precache-manifest.7c48e96417f2ec9e06ac786eac5571e7.js
@@ -1,23 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "a5d56c79fa125d6cfa5822fcda31916b",
+    "revision": "0bef74a09c562a1fa401787d9acc3ec3",
     "url": "/portfolio/index.html"
   },
   {
-    "revision": "79d9fbb22e30dc588c3a",
-    "url": "/portfolio/static/css/main.c7025b2c.chunk.css"
+    "revision": "cf848703651b90c76a1d",
+    "url": "/portfolio/static/css/main.50aa1e74.chunk.css"
   },
   {
     "revision": "029f9f22210d6fa6c700",
     "url": "/portfolio/static/js/2.ae89fda4.chunk.js"
   },
   {
-    "revision": "79d9fbb22e30dc588c3a",
+    "revision": "cf848703651b90c76a1d",
     "url": "/portfolio/static/js/main.cb410810.chunk.js"
   },
   {
     "revision": "76aa8cbd67b5c937ec2c",
     "url": "/portfolio/static/js/runtime-main.e92c632a.js"
+  },
+  {
+    "revision": "316bdc678a8168efe8db399bbbc2ba9d",
+    "url": "/portfolio/static/media/17526.316bdc67.ttf"
   },
   {
     "revision": "eff2ed0b7d6afac931ff62f2b33b7d07",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.6befe81cf2a095fffd722b7fd92c508c.js"
+  "/portfolio/precache-manifest.7c48e96417f2ec9e06ac786eac5571e7.js"
 );
 
 self.addEventListener('message', (event) => {
